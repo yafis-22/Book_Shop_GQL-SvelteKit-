91f974e0248e67b58762321dc868a55252d6baf2
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2023-12-14 21:28:43 +0530

    User register done

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,46 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Route to get a user by ID
+// Register a new user
+router.post('/register', async (req, res) => {
+  try {
+    const userData = await fs.readFile(userDataPath, 'utf8');
+    const users = JSON.parse(userData);
+
+    const { username, password, email } = req.body;
+
+    // Check if the username is already taken
+    const isUsernameTaken = users.some((user) => user.username === username);
+    if (isUsernameTaken) {
+      return res.status(400).send('Username is already taken');
+    }
+
+    // Check if the email is already taken
+    const isEmailTaken = users.some((user) => user.email === email);
+    if (isEmailTaken) {
+      return res.status(400).send('Email is already taken');
+    }
+
+    // Create a new user object
+    const newUser = {
+      id: users.length + 1,
+      username,
+      password,
+      email,
+    };
+
+    users.push(newUser);
+
+    // Write the updated user data back to userData.json
+    await fs.writeFile(userDataPath, JSON.stringify(users, null, 2));
+    res.json({ message: 'New user is registered', user: newUser });
+  } catch (err) {
+    console.error('Error registering user:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Get a user by ID
 router.get('/:id', async (req, res) => {
   const userId = parseInt(req.params.id);
 
