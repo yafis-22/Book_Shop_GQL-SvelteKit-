2679acd9773c2f0778cab9c4a2fb9de259d7f04c
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2023-12-16 23:22:47 +0530

    Added return book file

--- controllers/returnBook.js
+++ controllers/returnBook.js
@@ -2,13 +2,13 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs/promises';
 import path from 'path';
+import { lendBook } from './lendBook.js';
 
 // Get the directory name using import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const lendBook = async (req, res) => {
-    
+export const returnBook = async (req, res) => {
   try {
     const { bookId } = req.body;
 
@@ -27,6 +27,16 @@ export const lendBook = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
+    // Find the book in the user's lent books
+    const lentBookIndex = user.lentBooks.findIndex((lentBook) => lentBook.bookId === bookId);
+
+    if (lentBookIndex === -1) {
+      return res.status(404).json({ message: 'Book not found in the user\'s lent books' });
+    }
+
+    // Retrieve the lent book details
+    const lentBook = user.lentBooks[lentBookIndex];
+
     // Read books data from booksData.json
     const booksDataPath = path.join(__dirname, '../data/booksData.json');
     const booksData = await fs.readFile(booksDataPath, 'utf8');
@@ -39,37 +49,42 @@ export const lendBook = async (req, res) => {
       return res.status(404).json({ message: 'Book not found' });
     }
 
-    // Check if the book is available
-    if (book.quantity <= 0) {
-      return res.status(400).json({ message: 'Book not available for lending' });
-    }
-
-    // Calculate charges
+    // Calculate charges until the return date
     const lendingPrice = book.lendingPrice;
+    const days = calculateDaysDifference(lentBook.timestamp, new Date().toISOString());
     const initialCharge = lendingPrice;
+    const additionalCharge = days > 9 ? 5 * (days - 9) : 0;
+    const totalCharge = initialCharge + additionalCharge;
 
-    // Update user data
-    user.lentBooks.push({
-      bookId,
-      initialCharge,
-      timestamp: new Date().toISOString(),
-    });
+    // Clear the lent book from the user's lentBooks array
+    user.lentBooks.splice(lentBookIndex, 1);
 
-    // Update book data
-    book.quantity--;
+    // Increase the book quantity
+    book.quantity++;
 
     // Write the updated data back to userData.json and booksData.json
     await fs.writeFile(userDataPath, JSON.stringify(users, null, 2));
     await fs.writeFile(booksDataPath, JSON.stringify(books, null, 2));
 
     res.json({
-      message: 'Book lent successfully',
+      message: 'Book returned successfully',
       chargeDetails: {
         initialCharge,
+        additionalCharge,
+        totalCharge,
+        days,
       },
     });
   } catch (err) {
-    console.error('Error lending book:', err);
+    console.error('Error returning book:', err);
     res.status(500).send('Internal Server Error');
   }
 };
+
+// Function to calculate the difference in days between two dates
+function calculateDaysDifference(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const timeDifference = Math.abs(end - start);
+  return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+}
--- routes/books.js
+++ routes/books.js
@@ -8,6 +8,7 @@ import { lendBook } from '../controllers/lendBook.js';
 import { addBook } from '../controllers/addBook.js';
 import { updateBook } from '../controllers/updateBook.js'; 
 import { deleteBook } from '../controllers/deleteBook.js';
+import { returnBook } from '../controllers/returnBook.js';
 
 const router = express.Router();
 
@@ -17,14 +18,16 @@ const __dirname = dirname(__filename);
 
 // Books data file path
 const booksDataPath = path.join(__dirname, '../data/booksData.json');
-// User data file path
-const userDataPath = path.join(__dirname, '../data/userData.json');
+
 
 // Add a new book (only accessible by admin)
 router.post('/add', isAdmin, addBook); 
 
 // Lend a book
 router.post('/lend', authenticateUser, lendBook);
+
+// Return a lent book
+router.post('/return', authenticateUser, returnBook);
 
 // Update a book (only accessible by admin)
 router.put('/:id', isAdmin, updateBook);
