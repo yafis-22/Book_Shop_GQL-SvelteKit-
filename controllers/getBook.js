e2b45443372e5c936330fc1b0d806afec2a1ffad
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2023-12-24 16:05:44 +0530

    Added get book controller

---
+++ controllers/getBook.js
@@ -0,0 +1,71 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs/promises';
+import path from 'path';
+
+// Get the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const booksDataPath = path.join(__dirname, '../data/booksData.json');
+const booksData = await fs.readFile(booksDataPath, 'utf8');
+
+export const getBooks = async (req, res) => {
+    try {
+      const { title, category, author } = req.query;
+  
+      let allBooks = JSON.parse(booksData);
+  
+      if (title) {
+        allBooks = allBooks.filter((book) =>
+          book.title.toLowerCase().includes(title.toLowerCase())
+        );
+      }
+      if (category) {
+        allBooks = allBooks.filter((book) =>
+          book.category.toLowerCase() === category.toLowerCase()
+        );
+      }
+  
+      if (author) {
+        allBooks = allBooks.filter((book) =>
+          book.author.toLowerCase().includes(author.toLowerCase())
+        );
+      }
+  
+      res.json({
+        message: 'All books retrieved successfully',
+        books: allBooks,
+      });
+    } catch (err) {
+      console.error('Error reading books data:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  };
+
+  export const getBooksByCategory = async (req, res) => {
+    const categoryParam = req.params.category;
+  
+    try {
+      let books = JSON.parse(booksData);
+  
+      // If a category is specified, filter books by that category
+      if (categoryParam) {
+        const categoryBooks = books.filter(
+          (book) => book.category === categoryParam
+        );
+        res.json({
+          message: `Books in the category ${categoryParam} retrieved successfully`,
+          data: categoryBooks,
+        });
+      } else {
+        res.json({
+          message: 'All books retrieved successfully',
+          data: books,
+        });
+      }
+    } catch (err) {
+      console.error('Error reading books data:', err);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
--- routes/books.js
+++ routes/books.js
@@ -1,24 +1,13 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import express from 'express';
-import fs from 'fs/promises';
-import path from 'path';
 import { isAdmin, authenticateUser } from '../middlewares/authMiddleware.js';
 import { lendBook } from '../controllers/lendBook.js';
 import { addBook } from '../controllers/addBook.js';
 import { updateBook } from '../controllers/updateBook.js'; 
 import { deleteBook } from '../controllers/deleteBook.js';
 import { returnBook } from '../controllers/returnBook.js';
+import { getBooks, getBooksByCategory } from '../controllers/getBook.js';
 
 const router = express.Router();
-
-// Get the directory name using import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Books data file path
-const booksDataPath = path.join(__dirname, '../data/booksData.json');
-
 
 // Add a new book (only accessible by admin)
 router.post('/add', isAdmin, addBook); 
@@ -36,59 +25,8 @@ router.put('/:id', isAdmin, updateBook);
 router.delete('/:id', isAdmin, deleteBook);
 
 // Get all books
-router.get('/', async (req, res) => {
-  try {
-    const { title, category, author } = req.query;
+router.get('/', getBooks);
 
-    // Read user data from bookData.json
-    const booksData = await fs.readFile(booksDataPath, 'utf8');
-    let allBooks = JSON.parse(booksData);
-
-    if (title) {
-      allBooks = allBooks.filter((book) =>
-        book.title.toLowerCase().includes(title.toLowerCase())
-      );
-    }
-    if (category) {
-      allBooks = allBooks.filter((book) =>
-        book.category.toLowerCase() === category.toLowerCase()
-      );
-    }
-
-    if (author) {
-      allBooks = allBooks.filter((book) =>
-        book.author.toLowerCase().includes(author.toLowerCase())
-      );
-    }
-    res.json({
-      message: 'All books retrieved successfully',
-      books: allBooks,
-    });
-  } catch (err) {
-    console.error('Error reading user data:', err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-router.get('/:category?', async (req, res) => {
-  const categoryParam = req.params.category;
-
-  try {
-    // Read books data from booksData.json
-    const booksData = await fs.readFile(booksDataPath, 'utf8');
-    let books = JSON.parse(booksData);
-
-    // If a category is specified, filter books by that category
-    if (categoryParam) {
-      const categoryBooks = books.filter((book) => book.category === categoryParam);
-      res.json({ message: `Books in the category ${categoryParam} retrieved successfully`, data: categoryBooks });
-    } else {
-      res.json({ message: 'All books retrieved successfully', data: books });
-    }
-  } catch (err) {
-    console.error('Error reading books data:', err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+router.get('/:category?', getBooksByCategory);
   
 export default router;
