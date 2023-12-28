b98f2ac366e3232cee84e0b4a7dc1cbea4245e80
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2023-12-28 13:58:56 +0530

    Updated delete user

--- controllers/userController/deleteUser.js
+++ controllers/userController/deleteUser.js
@@ -4,7 +4,18 @@ export const deleteUser = async (req, res) => {
     try {
       // Fetch user ID from the authenticated user's token
       const requestingUserId = req.login.id;
-  
+      
+      // Fetch the user data
+      const user = await userModel.getUserById(requestingUserId);
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+
+      // Check if the user has any lent books
+      if (user.lentBooks.length > 0) {
+          return res.status(400).json({ message: 'You need to return all books before deleting your account', data:user.lentBooks });
+      }
+
       // Remove the user from the array
       const deletedUser = await userModel.softDeleteUser(requestingUserId);
       
