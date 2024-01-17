4ecb2419c9634ba9d76da57c1b3aa68d0bc71c02
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-01-17 13:14:36 +0530

    Updated fetch admins using sequelize modal

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import { isAdmin } from '../middlewares/authMiddleware.js';
-import * as userModel from '../models/userModal.js';
+import { Admin } from '../models/adminModal.js';
 
 const router = express.Router();
 
 // Admin routes
 router.get('/', isAdmin, async (req, res) => {
   try {
-    // Read admin data from adminData.json
-    const adminData = await userModel.getAdmins();
-    if (adminData) {
-      res.json(adminData);
+    // Fetch all admin users using Sequelize
+    const adminUsers = await Admin.findAll();
+
+    if (adminUsers.length > 0) {
+      res.json(adminUsers);
     } else {
       res.status(404).send('Admin data not found');
     }
-    
   } catch (err) {
     console.error('Error reading admin data:', err);
     res.status(500).send('Internal Server Error');
