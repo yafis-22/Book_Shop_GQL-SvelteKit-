12745bba0750e9026743ff2796e76aafc30549e8
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-01-31 23:22:00 +0530

    Updated folder structure

--- db/sequelize.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('bookLend', 'postgres', 'Qwert@123', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-export default sequelize;
