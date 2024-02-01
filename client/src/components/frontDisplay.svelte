04b095cd2020e48f085a6dfea735b6da303855a9
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-02-01 14:54:59 +0530

    Added Front display

---
+++ client/src/components/frontDisplay.svelte
@@ -0,0 +1,53 @@
+<script>
+    
+  </script>
+  
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 text-center">
+          <h2>Our monthly picks:</h2>
+          <p>Buy 3 or more and get a special discount</p>
+          <button class="btn btn-primary">Learn more</button>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="row g-0">
+              <div class="col-md-6">
+                <img class="card-img" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTuwIVgNXdfsXqFjytVZYcw1SN4SdtCDTmwZopiASdnffYt_K1J" alt="Quintessence by Jess Redman">
+              </div>
+              <div class="col-md-6">
+                <div class="card-body d-flex flex-column justify-content-between">
+                  <h3 class="card-title">Quintessence</h3>
+                  <div>
+                    <p class="card-text">By Jess Redman</p>
+                    <p class="card-text">$19.99</p>
+                    <p class="card-text">Three months ago, twelve-year-old Alma moved to the town of Four Points. Her panic attacks started a week later—and they haven’t stopped—even though she told her parents that they did. And every day she feels less and less like herself.</p>
+                  </div>
+                  <button class="btn btn-primary btn-sm align-self-end">Lend</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  
+  <style>
+    main {
+      margin-top: 20px;
+      background-color: #F5F5F5;
+    }
+  
+    .card {
+      margin-top: 30px;
+      padding: 20px;
+    }
+  
+    .card-img {
+      max-width: 100%;
+      height: auto;
+    }
+  </style>
+  
--- client/src/components/homePage.svelte
+++ client/src/components/homePage.svelte
@@ -1,9 +1,11 @@
 <script>
     import Header from "./header.svelte";
     import Footer from "./footer.svelte";
+    import FrontDisplay from "./frontDisplay.svelte";
 </script>
 
 <main>
     <Header />
+    <FrontDisplay />
     <Footer />
 </main>
