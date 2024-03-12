8f94fa24eabcea922f4ffbca466ba831b90c76e7
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-03-12 23:08:14 +0530

    Replaced the Rest API implementation with GraphQL for getting books by category

--- client/src/components/bookCategories.svelte
+++ client/src/components/bookCategories.svelte
@@ -3,8 +3,8 @@
   import BookCard from './bookCard.svelte';
   import Header from './header.svelte';
   import Footer from './footer.svelte';
-  import Pagination from './Pagination.svelte';
-  import Sorting from './Sorting.svelte';
+  import Pagination from './pagination.svelte';
+  import Sorting from './sorting.svelte';
 
   export let category;
 
@@ -16,12 +16,46 @@
 
   const fetchBooksByCategory = async () => {
     try {
-      const response = await fetch(`http://localhost:3002/api/v1/books/category/${category}?page=${currentPage}&pageSize=12&sortField=${sortField}&sortOrder=${sortOrder}`);
+      const response = await fetch(`http://localhost:4000/graphql`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            query GetBooksByCategory($category: String, $page: Int, $pageSize: Int, $sortField: String, $sortOrder: String) {
+              getBooksByCategory(category: $category, page: $page, pageSize: $pageSize, sortField: $sortField, sortOrder: $sortOrder) {
+                message
+                data {
+                  id
+                  title
+                  author
+                  lendingPrice
+                  imageSrc
+                }
+                totalPages
+              }
+            }
+          `,
+          variables: {
+            category: category,
+            page: currentPage,
+            pageSize: 12,
+            sortField: sortField,
+            sortOrder: sortOrder,
+          },
+        }),
+      });
+      
       if (response.ok) {
-        const data = await response.json();
-        books = data.books;
-        totalPages = data.totalPages || 1;
-        console.log("books", books)
+        const { data, errors } = await response.json();
+        if (!errors && data && data.getBooksByCategory) {
+          books = data.getBooksByCategory.data|| [];
+          totalPages = data.getBooksByCategory.totalPages || 1;
+          console.log("books", books)
+        } else {
+          console.error(`Error fetching books in category ${category}:`, errors || 'Invalid response format');
+        }
       } else {
         console.error(`Error fetching books in category ${category}:`, response.statusText);
       }
@@ -39,24 +73,25 @@
     await fetchBooksByCategory();
   };
 
-  $: if (category) {
-    books = [];
-    currentPage = 1;
-    totalPages = 1;
-    sortField = '';
-    sortOrder = '';
-    fetchBooksByCategory();
-  }
+  // $: if (category) {
+  //   books = [];
+  //   currentPage = 1;
+  //   totalPages = 1;
+  //   sortField = '';
+  //   sortOrder = '';
+  //   fetchBooksByCategory();
+  // }
 
   onMount(() => {
     fetchBooksByCategory();
   });
 
-  afterUpdate(() => {
-    fetchBooksByCategory();
-  });
+  // afterUpdate(() => {
+  //   fetchBooksByCategory();
+  // });
 
 </script>
+
 
 <Header />
 <h2 class="m-3">{category} Books</h2>
