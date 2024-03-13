9248f69da252fe5da4cd7c756fb86b9941e3ef21
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-03-13 12:45:14 +0530

    Fixed minor bug related to undefined description

--- client/src/components/bookDetails.svelte
+++ client/src/components/bookDetails.svelte
@@ -33,6 +33,7 @@
                   author
                   category
                   quantity
+                  description
                   lendingPrice
                   imageSrc
                 }
