e0b4f704d3245eae6cc5a465dd9f2bb06657e806
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-03-14 11:34:54 +0530

    Changed route structure

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,13 +1,54 @@
 <script>
-   import Main from "../components/main.svelte"
- </script>
- 
- <main>
-   <Main />
- </main>
+   import { Router, Route } from "svelte-routing";
+   import HomePage from "../components/homePage.svelte";
+   import UserLogin from "../components/userLogin.svelte";
+   import UserRegistrationForm from "../components/userRegistrationForm.svelte";
+   import BookManagement from "../components/bookManagement.svelte";
+   import BookDetails from "../components/bookDetails.svelte";
+   import AdminDashboard from "../components/adminDashboard.svelte";
+   import UserDashboard from "../components/userDashboard.svelte";
+   import BookCategories from "../components/bookCategories.svelte";
+   import Contact from "../components/contact.svelte";
+   import SearchResults from "../components/searchResults.svelte";
+</script>
+
+<main>
+   <Router>
+      <Route path="/">
+         <HomePage />
+      </Route>
+      <Route path="/login">
+         <UserLogin />
+      </Route>
+      <Route path="/users">
+         <UserRegistrationForm />
+      </Route>
+      <Route path="/books">
+         <BookManagement />
+      </Route>
+      <Route path="/books/:id" let:params>
+         <BookDetails bookId={params.id} />
+      </Route>
+      <Route path="/books/category/:category" let:params>
+         <BookCategories category={params.category} />
+      </Route>
+      <Route path="/admins/me">
+         <AdminDashboard />
+      </Route>
+      <Route path="/users/me">
+         <UserDashboard />
+      </Route>
+      <Route path="/contact">
+         <Contact />
+      </Route>
+      <Route path="/q">
+         <SearchResults />
+      </Route>
+   </Router>
+</main>
 
 <style>
    main {
-      background-color: #F5F5F5;
+      background-color: #f5f5f5;
    }
 </style>
