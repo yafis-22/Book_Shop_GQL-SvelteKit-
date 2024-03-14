3f1b5c8d8e2c97b125f4b091d66ce6f2318b6a53
Mohd Yafis Khan <mohd.yafis.khan@indxtechnology.com>
2024-03-14 12:58:19 +0530

    Fixed import names

--- client/src/components/contact.svelte
+++ client/src/components/contact.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Header from './Header.svelte';
-    import Footer from './Footer.svelte';
+    import Header from './header.svelte';
+    import Footer from './footer.svelte';
 </script>
 
 <div class="contactbody">
